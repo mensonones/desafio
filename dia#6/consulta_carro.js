7a7f01121da6da047128dd27131c9dbaaad2442b
Emerson Vieira <mensones.1@gmail.com>
2017-04-07 16:13:54 -0300

    six day

---
+++ dia#6/consulta_carro.js
@@ -0,0 +1,28 @@
+/**
+ * Created by emerson on 07/04/17.
+ */
+
+var http = require('http');
+
+http.get('http://fipeapi.appspot.com/api/1/carros/veiculo/21/4828/2013-1.json', function (res) {
+    var body = '';
+
+    console.log('Response: ' + res.statusCode);
+    console.log('----------------------------');
+
+    res.on('data', function (chunk) {
+        body+=chunk;
+    });
+
+    res.on('end', function () {
+        var json = body;
+
+        console.log(json);
+    });
+
+    res.on('err', function (e) {
+        console.log('O erro é: ' + e);
+    });
+
+}).end();
+
